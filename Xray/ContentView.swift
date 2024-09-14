5cf41c616f05d5600e7eecf41c31780435f3cd76
pan <xiaosheng@cloud-unicom.cn>
2024-09-14 11:21:26 +0800

    初始化

---
+++ Xray/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Xray
+//
+//  Created by pan on 2024/9/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ Xray/XrayApp.swift
@@ -0,0 +1,17 @@
+//
+//  XrayApp.swift
+//  Xray
+//
+//  Created by pan on 2024/9/14.
+//
+
+import SwiftUI
+
+@main
+struct XrayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
