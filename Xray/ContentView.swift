00bb618ea4bde83f3158749cbd92b6c887009979
pan <xiaosheng@cloud-unicom.cn>
2024-09-29 10:32:31 +0800

    二维码

--- Xray/ContentView.swift
+++ Xray/ContentView.swift
@@ -9,12 +9,14 @@ import SwiftUI
 
 @MainActor
 struct ContentView: View {
-    @EnvironmentObject var packetTunnelManager: PacketTunnelManager // 监听 PacketTunnelManager 的状态
+    @EnvironmentObject var packetTunnelManager: PacketTunnelManager
     @State private var clipboardText: String = ""
     @State private var idText: String = ""
     @State private var ipText: String = ""
     @State private var portText: String = ""
-
+    @State private var isShowingShareModal = false // 控制弹窗显示
+    @State private var showClipboardEmptyAlert = false // 用于控制显示空剪贴板的提示
+    
     var body: some View {
         VStack(alignment: .leading) {
             VStack(alignment: .leading) {
@@ -22,30 +24,36 @@ struct ContentView: View {
                 InfoRow(label: "ID:", text: idText)
                 InfoRow(label: "IP地址:", text: Util.maskIPAddress(ipText))
                 InfoRow(label: "端口:", text: portText)
-                
-                ConnectedDurationView()
-                
-                TrafficStatsView(trafficPort: Constant.trafficPort)
 
+                ConnectedDurationView()
+
+                TrafficStatsView(trafficPort: Constant.trafficPort)
             }
             .padding()
 
             Spacer()
-            
-            VPNControlView(sock5Text: Constant.sock5Port,trafficPortText: Constant.trafficPort, connectIfValidPort: { sock5Port,trafficPort  in
-                await connectVPN(sock5Port:sock5Port,trafficPort:trafficPort)
-            })
 
-            // 从剪贴板粘贴按钮
+            // 剪贴板和分享配置按钮
             HStack {
                 Button("从剪贴板粘贴") {
                     handlePasteFromClipboard()
                 }
                 .buttonStyle(ActionButtonStyle(color: .blue))
-                .padding(.horizontal)
+
+                Spacer() // 在按钮之间添加空隙
+
+                Button("分享配置") {
+                    isShowingShareModal = true
+                }
+                .buttonStyle(ActionButtonStyle(color: .yellow))
             }
+            .padding(.horizontal)
             .padding(.top, 20)
 
+            VPNControlView(sock5Text: Constant.sock5Port, trafficPortText: Constant.trafficPort) { sock5Port, trafficPort in
+                await connectVPN(sock5Port: sock5Port, trafficPort: trafficPort)
+            }
+            
             // 版本号显示
             HStack {
                 Spacer()
@@ -57,10 +65,17 @@ struct ContentView: View {
         .onAppear {
             loadDataFromUserDefaults()
         }
+        // 弹出 ShareButton 作为模态视图
+        .sheet(isPresented: $isShowingShareModal) {
+            ShareModalView(isShowing: $isShowingShareModal) // 弹出新的视图
+        }// 提示剪贴板为空
+        .alert(isPresented: $showClipboardEmptyAlert) {
+            Alert(title: Text("剪贴板为空"), message: Text("没有从剪贴板获取到内容"), dismissButton: .default(Text("确定")))
+        }
     }
 
     // MARK: - VPN Connection
-    private func connectVPN(sock5Port: Int,trafficPort:Int) async {
+    private func connectVPN(sock5Port: Int, trafficPort: Int) async {
         do {
             if clipboardText.isEmpty {
                 guard let savedContent = Util.loadFromUserDefaults(key: "clipboardContent"), !savedContent.isEmpty else {
@@ -68,13 +83,13 @@ struct ContentView: View {
                 }
                 clipboardText = savedContent
             }
-            
-            let configData = try Configuration().buildConfigurationData(inboundPort: sock5Port, trafficPort:trafficPort ,config: clipboardText)
-            
+
+            let configData = try Configuration().buildConfigurationData(inboundPort: sock5Port, trafficPort: trafficPort, config: clipboardText)
+
             guard let mergedConfigString = String(data: configData, encoding: .utf8) else {
                 throw NSError(domain: "ConfigDataError", code: -1, userInfo: [NSLocalizedDescriptionKey: "无法将配置数据转换为字符串"])
             }
-//            print(mergedConfigString)
+
             try await packetTunnelManager.start(sock5Port: sock5Port, config: mergedConfigString)
         } catch {
             print("连接 VPN 时出错: \(error.localizedDescription)")
@@ -91,12 +106,20 @@ struct ContentView: View {
     // MARK: - Clipboard Handling
     private func handlePasteFromClipboard() {
         if let clipboardContent = Util.pasteFromClipboard() {
-            let storedContent = Util.loadFromUserDefaults(key: "clipboardContent")
-            if clipboardContent != storedContent {
-                clipboardText = clipboardContent
-                Util.saveToUserDefaults(value: clipboardContent, key: "clipboardContent")
-                Util.parseContent(clipboardContent, idText: &idText, ipText: &ipText, portText: &portText)
+            if !clipboardContent.isEmpty {
+                let storedContent = Util.loadFromUserDefaults(key: "clipboardContent")
+                if clipboardContent != storedContent {
+                    clipboardText = clipboardContent
+                    Util.saveToUserDefaults(value: clipboardContent, key: "clipboardContent")
+                    Util.parseContent(clipboardContent, idText: &idText, ipText: &ipText, portText: &portText)
+                }
+            } else {
+                print("剪贴板内容为空字符串")
+                showClipboardEmptyAlert = true
             }
+        } else {
+            print("剪贴板内容为空")
+            showClipboardEmptyAlert = true // 剪贴板为空时显示提示
         }
     }
 }
---
+++ Xray/ShareButton.swift
@@ -0,0 +1,87 @@
+//
+//  ShareButton.swift
+//  Xray
+//
+//  Created by pan on 2024/9/29.
+//
+
+import SwiftUI
+import CoreImage.CIFilterBuiltins
+
+struct ShareModalView: View {
+    @Binding var isShowing: Bool // 控制弹窗的显示与关闭
+    @State private var shareLink: String = ""
+    @State private var qrCodeImage: UIImage?
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                // 显示生成的链接并处理换行
+                if !shareLink.isEmpty {
+                    Text(shareLink)
+                        .font(.body)
+                        .padding()
+                        .lineLimit(nil) // 允许换行
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                }
+
+                // 显示二维码
+                if let qrCodeImage = qrCodeImage {
+                    Image(uiImage: qrCodeImage)
+                        .resizable()
+                        .interpolation(.none) // 避免图片缩放时模糊
+                        .frame(width: 200, height: 200) // 更大的尺寸
+                        .padding()
+                } else {
+                    Text("正在生成二维码...")
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                }
+
+                Spacer()
+            }
+            .navigationBarTitle("分享配置", displayMode: .inline)
+            .navigationBarItems(trailing: Button("关闭") {
+                isShowing = false
+            })
+            .onAppear {
+                generateQRCode()
+            } // 在视图显示时自动生成二维码
+        }
+    }
+
+    // 自动生成二维码的方法
+    private func generateQRCode() {
+        guard let link = Util.loadFromUserDefaults(key: "clipboardContent"), !link.isEmpty else {
+            print("无法生成二维码，因为没有可用的内容")
+            return
+        }
+
+        guard let data = link.data(using: .utf8) else {
+            print("无法将内容转换为数据")
+            return
+        }
+
+        shareLink = link
+
+        let filter = CIFilter.qrCodeGenerator()
+        filter.setValue(data, forKey: "inputMessage")
+
+        guard let qrCodeImage = filter.outputImage else {
+            print("无法生成二维码图像")
+            return
+        }
+
+        // 将二维码放大为更高分辨率
+        let transform = CGAffineTransform(scaleX: 20, y: 20) // 放大二维码的分辨率
+        let scaledQRCodeImage = qrCodeImage.transformed(by: transform)
+
+        let context = CIContext()
+        if let cgImage = context.createCGImage(scaledQRCodeImage, from: scaledQRCodeImage.extent) {
+            let uiImage = UIImage(cgImage: cgImage)
+            self.qrCodeImage = uiImage
+        } else {
+            print("无法生成二维码的 CGImage")
+        }
+    }
+}
