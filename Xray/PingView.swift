9914ebf307a46ad408948483c48ae7505d26edd0
pan <xiaosheng@cloud-unicom.cn>
2024-10-22 17:06:12 +0800

    优化获取网速页面

--- Xray/PingView.swift
+++ Xray/PingView.swift
@@ -14,25 +14,30 @@ struct PingView: View {
     @EnvironmentObject var packetTunnelManager: PacketTunnelManager // 引入 PacketTunnelManager
     @State private var pingSpeed: Int = 0
     @State private var isPingFetched: Bool = false // 全局变量，初始为 false
+    @State private var isLoading: Bool = false // 新增，控制是否显示加载动画
 
     var body: some View {
         VStack {
-            // 显示 Ping 信息
-            if isPingFetched  {
-                HStack {
-                    Text("Ping网速:")
-                    Text("\(pingSpeed)").foregroundColor(pingSpeedColor(pingSpeed)).font(.headline)
-                    Text("ms").foregroundColor(.black) //
-                }
+            // 如果正在加载，显示 “正在获取网速...” 文本
+            if isLoading {
+                Text("正在获取网速...")
             } else {
-                if(packetTunnelManager.status == .disconnected){
-                    Text("点击获取网速")
-                        .foregroundColor(.blue)
-                        .onTapGesture {
-                            requestPing() // 点击时调用 requestPing
-                        }
+                // 显示 Ping 信息
+                if isPingFetched {
+                    HStack {
+                        Text("Ping网速:")
+                        Text("\(pingSpeed)").foregroundColor(pingSpeedColor(pingSpeed)).font(.headline)
+                        Text("ms").foregroundColor(.black)
+                    }
+                } else {
+                    if packetTunnelManager.status == .disconnected {
+                        Text("点击获取网速")
+                            .foregroundColor(.blue)
+                            .onTapGesture {
+                                requestPing() // 点击时调用 requestPing
+                            }
+                    }
                 }
-                
             }
         }
         .padding(.top, 10)
@@ -40,40 +45,45 @@ struct PingView: View {
 
     // Ping 请求逻辑
     private func requestPing() {
-        do {
-            guard let savedContent = Util.loadFromUserDefaults(key: "clipboardContent"), !savedContent.isEmpty else {
-                throw NSError(domain: "ContentView", code: 0, userInfo: [NSLocalizedDescriptionKey: "没有可用的配置，且剪贴板内容为空"])
-            }
+        // 开始加载，显示 loading 样式
+        isLoading = true
 
-            let configData = try Configuration().buildConfigurationData(config: savedContent)
+        Task {
+            do {
+                guard let savedContent = Util.loadFromUserDefaults(key: "clipboardContent"), !savedContent.isEmpty else {
+                    throw NSError(domain: "ContentView", code: 0, userInfo: [NSLocalizedDescriptionKey: "没有可用的配置，且剪贴板内容为空"])
+                }
 
-            guard let mergedConfigString = String(data: configData, encoding: .utf8) else {
-                throw NSError(domain: "ConfigDataError", code: -1, userInfo: [NSLocalizedDescriptionKey: "无法将配置数据转换为字符串"])
-            }
+                let configData = try Configuration().buildConfigurationData(config: savedContent)
 
-            let fileUrl = try Util.createConfigFile(with: mergedConfigString)
-                    
-            guard let sock5PortString = Util.loadFromUserDefaults(key: "sock5Port"),
-                  let sock5PortString = Int(sock5PortString) else {
-                throw NSError(domain: "ConfigurationError", code: 0, userInfo: [NSLocalizedDescriptionKey: "无法从 UserDefaults 加载端口或端口格式不正确"])
+                guard let mergedConfigString = String(data: configData, encoding: .utf8) else {
+                    throw NSError(domain: "ConfigDataError", code: -1, userInfo: [NSLocalizedDescriptionKey: "无法将配置数据转换为字符串"])
+                }
+
+                let fileUrl = try Util.createConfigFile(with: mergedConfigString)
+                        
+                guard let sock5PortString = Util.loadFromUserDefaults(key: "sock5Port"),
+                      let sock5Port = Int(sock5PortString) else {
+                    throw NSError(domain: "ConfigurationError", code: 0, userInfo: [NSLocalizedDescriptionKey: "无法从 UserDefaults 加载端口或端口格式不正确"])
+                }
+                
+                let pingRequest = try createPingRequest(configPath: fileUrl.path(), sock5Port: sock5Port)
+                let pingBase64String = try JSONEncoder().encode(pingRequest).base64EncodedString()
+
+                // 调用 LibXrayPing 并处理响应
+                let pingResponseBase64 = LibXrayPing(pingBase64String)
+                if let pingResult = await decodePingResponse(base64String: pingResponseBase64) {
+                    self.pingSpeed = pingResult
+                    self.isPingFetched = true
+                } else {
+                    print("Ping 解码失败")
+                }
+            } catch let error as NSError {
+                print("Ping 请求失败: \(error.localizedDescription)")
+            } catch {
+                print("发生了未知错误: \(error.localizedDescription)")
             }
-            
-            // 创建并发送 Ping 请求
-            let pingRequest = try createPingRequest(configPath: fileUrl.path(), sock5Port: sock5PortString)
-            let pingBase64String = try JSONEncoder().encode(pingRequest).base64EncodedString()
-            
-            // 调用 LibXrayPing 并处理响应
-            let pingResponseBase64 = LibXrayPing(pingBase64String)
-            if let pingResult = decodePingResponse(base64String: pingResponseBase64) {
-                self.pingSpeed = pingResult
-                self.isPingFetched = true
-            } else {
-                print("Ping 解码失败")
-            }
-        } catch let error as NSError {
-            print("Ping 请求失败: \(error.localizedDescription)")
-        } catch {
-            print("发生了未知错误: \(error.localizedDescription)")
+            self.isLoading = false // 停止加载
         }
     }
 
@@ -93,6 +103,7 @@ struct PingView: View {
     }
 
     // 创建 Ping 请求
+    @MainActor
     private func createPingRequest(configPath: String, sock5Port: Int) throws -> PingRequest {
         return PingRequest(
             datDir: Constant.assetDirectory.path,
@@ -104,7 +115,8 @@ struct PingView: View {
     }
 
     // 解码 Base64 响应并提取 "data" 字段中的网速
-    private func decodePingResponse(base64String: String) -> Int? {
+    @MainActor
+    private func decodePingResponse(base64String: String) async -> Int? {
         guard let decodedData = Data(base64Encoded: base64String),
               let decodedString = String(data: decodedData, encoding: .utf8),
               let jsonData = decodedString.data(using: .utf8) else {
